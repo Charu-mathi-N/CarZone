75a2869d26ff755eef0f90e03d9b8cea83b7721b
Charu-mathi-N <70507486+Charu-mathi-N@users.noreply.github.com>
2021-06-12 13:16:25 +0530

    Configure Admin page

--- pages/admin.py
+++ pages/admin.py
@@ -1,6 +1,15 @@
 from .models import Team
 from django.contrib import admin
+from django.utils.html import format_html
 
 # Register your models here.
 
-admin.site.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    def Photo(Self, object):
+        return format_html('<img src="{}" width="40" style= "border-radius: 50px;"/>'.format(object.photo.url))
+
+    list_display = ('id', 'Photo', 'first_name', 'designation', 'created_date')
+    list_display_links = ("id","Photo", "first_name")
+    search_fields = ("first_name", "designation")
+    list_filter = ('designation',)
+admin.site.register(Team, TeamAdmin)
