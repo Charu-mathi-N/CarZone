e5fe0aa64cc9351b634f08c4c6587a054980c1b8
Charu-mathi-N <70507486+Charu-mathi-N@users.noreply.github.com>
2021-07-01 11:31:16 +0530

    Add Car Features

--- cars/urls.py
+++ cars/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# app_name = "cars"
+
 urlpatterns = [
     path('', views.cars, name="cars"),
+    path('<int:id>', views.car_detail, name="car_detail"),
 ]
